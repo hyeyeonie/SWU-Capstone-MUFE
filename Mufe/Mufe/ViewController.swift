98b1d242215cd7b0c69a772e900ed98c86ebaf8d
hyeyeonie <shy020916@naver.com>
2025-05-14 16:50:15 +0900

    [add] #2 Codebase layout 초기 세팅

---
+++ Mufe/Mufe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mufe
+//
+//  Created by 신혜연 on 5/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
