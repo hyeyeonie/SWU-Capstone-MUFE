8d1730eb93e1a9f1a43e0bb36092565c724cb6a5
hyeyeonie <shy020916@naver.com>
2025-06-04 22:47:54 +0900

    [feat] #6 OnboardingViewController에 SelectTimeView 추가 및 nextButton 추가

--- Mufe/Mufe/Presentation/OnboardingViewController.swift
+++ Mufe/Mufe/Presentation/OnboardingViewController.swift
@@ -39,6 +39,21 @@ class OnboardingViewController: UIViewController {
     
     private let selectFestivalView = SelectFestivalView()
     private let selectDateView = SelectDateView()
+    private let selectTimeView = SelectTimeView()
+    
+    private let nextButton = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("다음으로", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        $0.layer.cornerRadius = 16
+        
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+        $0.layer.shadowRadius = 8
+        $0.layer.shadowOpacity = 0.4
+        $0.layer.masksToBounds = false
+    }
     
     var selectedFestivalName: String = "2025 뷰티풀민트라이프"
     var currentStep: FestivalProgressStep = .festivalSelection {
@@ -51,14 +66,27 @@ class OnboardingViewController: UIViewController {
             case .festivalSelection:
                 selectFestivalView.isHidden = false
                 selectDateView.isHidden = true
+                selectTimeView.isHidden = true
+
             case .dateSelection:
                 selectFestivalView.isHidden = true
                 selectDateView.isHidden = false
+                selectTimeView.isHidden = true
+
             case .timeSelection:
-                selectDateView.isHidden = false
+                selectFestivalView.isHidden = true
+                selectDateView.isHidden = true
+                selectTimeView.isHidden = false
+
             case .artistSelection:
+                selectFestivalView.isHidden = true
                 selectDateView.isHidden = false
+                selectTimeView.isHidden = true
             }
+            
+            selectTimeView.updateItems([
+                DateItem(day: "1일차", date: "1월 7일 토요일", enterTime: "13:00", leaveTime: "21:00")
+            ])
         }
     }
     
@@ -76,7 +104,7 @@ class OnboardingViewController: UIViewController {
     
     private func setStyle() {
         view.backgroundColor = .black
-        currentStep = .dateSelection
+        currentStep = .timeSelection
     }
     
     private func setUI() {
@@ -84,10 +112,11 @@ class OnboardingViewController: UIViewController {
             backButton,
             progressBar,
             titleLabel,
-            scrollView
+            scrollView,
+            nextButton
         )
         scrollView.addSubview(contentView)
-        contentView.addSubviews(selectFestivalView, selectDateView)
+        contentView.addSubviews(selectFestivalView, selectDateView, selectTimeView)
     }
     
     private func setLayout() {
@@ -112,7 +141,7 @@ class OnboardingViewController: UIViewController {
         scrollView.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(32)
             $0.horizontalEdges.equalToSuperview().inset(16)
-            $0.bottom.equalToSuperview()
+            $0.bottom.equalTo(nextButton.snp.top).offset(-16)
         }
         
         contentView.snp.makeConstraints {
@@ -128,6 +157,17 @@ class OnboardingViewController: UIViewController {
         selectDateView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
+        }
+        
+        selectTimeView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().offset(-24)
+            $0.height.equalTo(53)
         }
     }
 }
