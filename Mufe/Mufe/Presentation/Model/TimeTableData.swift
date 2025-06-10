387630694a84cc539d3fdd399ff930b1f77165b1
hyeyeonie <shy020916@naver.com>
2025-06-11 00:52:32 +0900

    [add] #12 ChatGPT에서 Response로 받아와 PersonalTimeTableViewController에 뿌릴 모델 추가

---
+++ Mufe/Mufe/Presentation/Model/TimeTableData.swift
@@ -0,0 +1,18 @@
+//
+//  TimeTableData.swift
+//  Mufe
+//
+//  Created by 신혜연 on 6/10/25.
+//
+
+import Foundation
+import UIKit
+
+struct TimetableData {
+    let artistName: String
+    let location: String
+    let startTime: String
+    let endTime: String
+    let duration: Int
+    let genre: String
+}
