02bc2c41f60eac1fdf0e1aeac5698732f4c96564
hyeyeonie <shy020916@naver.com>
2025-09-24 15:40:46 +0900

    [feat] 타이틀 문구 분기에 따라 구현

--- Mufe/Mufe/Presentation/ViewController/HomeViewController.swift
+++ Mufe/Mufe/Presentation/ViewController/HomeViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 import SnapKit
 import Then
 
+enum HomeViewState {
+    case emptyFestival
+    case beforeFestival
+    case dDayFestival
+    case afterFestival
+}
+
 final class HomeViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private var currentState: HomeViewState = .emptyFestival {
+        didSet { updateView() }
+    }
+    
     // MARK: - UI Components
-
+    
     private let emptyFestivalView = EmptyFestivalView()
+    private let beforeFestivalView = BeforeFestivalView()
+    private let dDayFestivalView = DdayFestivalView()
+    private let afterFestivalView = AfterFestivalView()
+    
+    private let titleLabel = UILabel().then {
+        $0.numberOfLines = 2
+        $0.textColor = .gray20
+        $0.customFont(.fxl_Medium)
+        $0.textAlignment = .left
+    }
     
     // MARK: - Life Cycle
     
@@ -25,6 +48,7 @@ final class HomeViewController: UIViewController {
         setUI()
         setLayout()
         setDelegate()
+        updateView()
     }
     
     // MARK: - Setup Methods
@@ -34,17 +58,74 @@ final class HomeViewController: UIViewController {
     }
     
     private func setUI() {
-        view.addSubviews(emptyFestivalView)
+        view.addSubviews(emptyFestivalView, beforeFestivalView, dDayFestivalView, afterFestivalView,
+                         titleLabel)
     }
     
     private func setLayout() {
-        emptyFestivalView.snp.makeConstraints{
-            $0.edges.equalToSuperview()
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(74)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        [beforeFestivalView, dDayFestivalView].forEach { view in
+            view.snp.makeConstraints {
+                $0.top.equalTo(titleLabel.snp.bottom)
+                $0.horizontalEdges.bottom.equalToSuperview()
+            }
+        }
+        
+        [emptyFestivalView, afterFestivalView].forEach { view in
+            view.snp.makeConstraints {
+                $0.edges.equalToSuperview()
+            }
         }
     }
     
     private func setDelegate() {
         emptyFestivalView.delegate = self
+    }
+    
+    private func updateView() {
+        emptyFestivalView.isHidden = currentState != .emptyFestival
+        beforeFestivalView.isHidden = currentState != .beforeFestival
+        dDayFestivalView.isHidden = currentState != .dDayFestival
+        afterFestivalView.isHidden = currentState != .afterFestival
+        
+        switch currentState {
+        case .beforeFestival:
+            titleLabel.isHidden = false
+            let remainingDays = 29
+            let text = "두근두근!\n페스티벌이 \(remainingDays)일 남았어요."
+            
+            let attributedText = NSMutableAttributedString(string: text)
+            
+            if let range = text.range(of: "\(remainingDays)") {
+                let nsRange = NSRange(range, in: text)
+                attributedText.addAttribute(.font, value: CustomUIFont.fxl_Bold.font, range: nsRange)
+            }
+            
+            titleLabel.attributedText = attributedText
+        case .dDayFestival:
+            titleLabel.isHidden = false
+            let artistName = "아사달"
+            let text = "\(artistName)의 공연\n재밌게 즐기고 계신가요?"
+            
+            let attributedText = NSMutableAttributedString(string: text)
+            
+            if let range = text.range(of: artistName) {
+                let nsRange = NSRange(range, in: text)
+                attributedText.addAttribute(.font, value: CustomUIFont.fxl_Bold.font, range: nsRange)
+            }
+            
+            titleLabel.attributedText = attributedText
+        default:
+            titleLabel.isHidden = true
+        }
+    }
+    
+    func updateState(_ newState: HomeViewState) {
+        currentState = newState
     }
 }
 
